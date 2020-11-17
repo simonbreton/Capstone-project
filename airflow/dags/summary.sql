643d25b5fef318d7834ff748b0d1767b4617d0e4
Simon breton <simon@blueprint-analytics.com>
2020-11-16 23:05:07 -0600

    Delete summary.sql

--- airflow/dags/summary.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO {{ var.json.env.project }}.{{ var.json.env.production }}.{{ var.json.env.summary }}
-SELECT *
-FROM `{{ var.json.env.project }}.{{ var.json.env.production }}.{{ var.json.env.fact }}`
-LEFT JOIN
-`{{ var.json.env.project }}.{{ var.json.env.production }}.{{ var.json.env.geo }}`
-USING 
-	(surrogate_keys)
-WHERE 
-	partitioned_key between'{{ execution_date.strftime("%Y-%m-%d") }}' and '{{ next_execution_date.strftime("%Y-%m-%d") }}'
