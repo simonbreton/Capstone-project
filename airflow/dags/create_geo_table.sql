f45ea94fbe9c796d93ce184e848357e879d32ddf
Simon breton <simon@blueprint-analytics.com>
2020-11-16 23:04:08 -0600

    Delete create_geo_table.sql

--- airflow/dags/create_geo_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS
-        {{ var.json.env.project }}.{{ var.json.env.production }}.{{ var.json.env.geo }}
-        (surrogate_keys STRING,
-        pickup_zone STRING,
-        pickup_borough STRING,
-        dropoff_zone STRING,
-        dropoff_borough STRING)
