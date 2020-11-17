73b6662711b99f7a528e90c867278c0ac8923516
Simon breton <simon@blueprint-analytics.com>
2020-11-16 23:04:59 -0600

    Delete quality_check_duplicate.sql

--- airflow/dags/quality_check_duplicate.sql
+++
@@ -1,8 +0,0 @@
-SELECT surrogate_keys,
-       count(*)
-FROM
-  {}
-WHERE
-  partitioned_key between'{{ execution_date.strftime("%Y-%m-%d") }}' and '{{ next_execution_date.strftime("%Y-%m-%d") }}'
-group by surrogate_keys
-having count(*) > 1;
